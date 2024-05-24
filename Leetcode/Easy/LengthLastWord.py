86f11c3b76e3f3d6335f9d051f8909cab5e59e0d
unknown <olibier.drk@gmail.com>
2024-05-24 07:20:53 +0200

    ajout easy leetcode

---
+++ Leetcode/Easy/LengthLastWord.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        r=len(s)-1
+        count=0
+
+        while s[r]==" " and r>=0:
+            r-=1
+
+        while s[r]!=" " and r>=0:
+            count+=1
+            r-=1
+
+        return count
+        
